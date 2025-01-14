167f2fc6fccf012ac0664f7374ee61afa5ec406f
Ylenia <yleniamighela@gmail.com>
2025-01-14 15:43:39 +0100

    added first part of logic

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// PRIMA PARTE
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l’utente deve inserire i numeri che ha visto precedentemente, nell’ordine che preferisce.
+// SECONDA PARTE
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+// PRIMA PARTE 
+
+// creo un array dove mi estrapola 5 numeri casuali
+
+// creo funzione asincrona (setInterval) per far partire il timer di 30 secondi
+
+
+   // terminato il tempo appaiono i 5 input dove l'utente inserirà i numeri
