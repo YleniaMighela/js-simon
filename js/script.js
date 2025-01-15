d872644af5cd13bb9096430deb37d761016a6764
Ylenia <yleniamighela@gmail.com>
2025-01-15 10:39:47 +0100

    added way to create new element to insert into the dom

--- js/script.js
+++ js/script.js
@@ -7,6 +7,11 @@
 // PRIMA PARTE 
 
 // seleziono elementi di input 
+const listaNumeri = document.getElementById('numbers-list');
+
+
+
+
 
 
 
@@ -14,15 +19,35 @@
 
 let numeriGenerati = [];
 
+// creo un ciclo for 
 for (let i = 0; i < 5; i++){
-
+   
+   // questo metodo mi permette di estrapolare i 50 numeri
    let numeriRandom = Math.floor(Math.random()* 50) + 1;
    
+  
+   // SE la variabile numeri Random include un numero generato già presente allora decrementa per rigenerarlo
    if(numeriGenerati.includes(numeriRandom)){
       i--;
-   } else {
-      numeriGenerati.push(numeriRandom);
    }
+   // ALTRIMENTI se non è presente aggiungilo nell'array
+   else {
+      numeriGenerati.push(numeriRandom);
+      
+      // per far sì che la lista di numeri risulti all'interno dell'HTML,aggiungo il metodo per creare e aggangiare i 'li'
+
+
+      //  aggiungo l'elemento "LI"" in nuona nuova variabile "lista"
+      let nuovoElementoLista = document.createElement('li');
+      
+      // ogni singolo elemento all'interno dell'array verrà associato ad un numero generato ed il contenuto sarà inserito nell'HTML
+      nuovoElementoLista.innerHTML = (numeriGenerati[i]);
+      
+      // mi permette di aggiungere l'elemento nel DOM
+      listaNumeri.appendChild(nuovoElementoLista);
+   }
+   
+   
 };
 
 console.log(numeriGenerati);
@@ -39,4 +64,4 @@ console.log(numeriGenerati);
 // creo funzione asincrona (setInterval) per far partire il timer di 30 secondi
 
 
-   // terminato il tempo appaiono i 5 input dove l'utente inserirà i numeri
+// terminato il tempo appaiono i 5 input dove l'utente inserirà i numeri
