8cb7cedb3bed2e195dc0fceff6df546e87f7e64a
Ylenia <yleniamighela@gmail.com>
2025-01-15 12:30:31 +0100

    created array for inserting user numbers

--- js/script.js
+++ js/script.js
@@ -4,17 +4,20 @@
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
 
-// PRIMA PARTE 
 
-// seleziono elementi di OUTPUT
+
+// seleziono elementi da prendere nell'HTML
 const listaNumeri = document.getElementById('numbers-list');
 
 const countdownDisplay = document.getElementById('countdown');
 
 const formDisplay = document.getElementById ('answers-form');
 
+const numInput = document.querySelectorAll ('input');
 
 
+
+// PRIMA PARTE 
 // creo un array vuoto dove mi inserirà i 5 numeri estrapolati casualmente
 
 let numeriGenerati = [];
@@ -25,7 +28,7 @@ for (let i = 0; i < 5; i++){
    // questo metodo mi permette di estrapolare i 50 numeri
    let numeriRandom = Math.floor(Math.random()* 50) + 1;
    
-  
+   
    // SE la variabile numeri Random include un numero generato già presente allora decrementa per rigenerarlo
    if(numeriGenerati.includes(numeriRandom)){
       i--;
@@ -35,8 +38,8 @@ for (let i = 0; i < 5; i++){
       numeriGenerati.push(numeriRandom);
       
       // per far sì che la lista di numeri risulti all'interno dell'HTML,aggiungo il metodo per creare e aggangiare i 'li'
-
-
+      
+      
       //  aggiungo l'elemento "LI"" in nuona nuova variabile "lista"
       let nuovoElementoLista = document.createElement('li');
       
@@ -58,27 +61,27 @@ console.log(numeriGenerati);
 
 // inizializzo i secondi
 
-let second = 2;
+let second = 5;
 
 // funzione asincrona
 
 const countdown = setInterval (() => {
-
+   
    // Se i secondi sono uguali a 0 allora ferma il countdown
-
+   
    if (second === 0) {
       clearInterval (countdown);
-
+      
       // quando i secondi sono arrivati a 0 aggiungi una classe che nasconde i numeri
-
+      
       listaNumeri.classList.add("d-none");
-
+      
       // una volta nascosti i numeri tirami fuori il form con gli input cancellando la classe display none
-
+      
       formDisplay.classList.remove("d-none");
-
+      
    }
-
+   
    // Altrimenti decrementa di uno fino e aggiornalo continuamente nell'HTML (innerHTML)
    else{
       countdownDisplay.innerHTML = --second;
@@ -87,9 +90,27 @@ const countdown = setInterval (() => {
 
 
 
+// SECONDA PARTE
 
 
+// creo una array vuoto, dove verrà aggiornato e aggiunto il valore(numero) inserito dall'utente nell'input
+let userArray = []; 
 
 
+// creo un evento in cui una volta che l'utente ha inserito i numeri il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-// terminato il tempo appaiono i 5 input dove l'utente inserirà i numeri
+formDisplay.addEventListener ('submit', (event) =>{
+   
+   // blocco l'aggiornamento
+   event.preventDefault();
+   
+   // creo un ciclo for 
+   for (let i = 0; i < numInput.length; i++){
+      
+      // dove aggiunge in singolo elemento (iesimo) all'interno dell'array
+      
+      userArray.push(numInput[i].value);
+   }
+   
+});
+console.log(userArray);
